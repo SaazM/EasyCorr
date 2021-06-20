c6edc666f07daeeb36c561a15759afcf9de402dc
Saaz <saaz.m@icloud.com>
2021-06-19 22:41:13 -0400

    added correlation model

--- EasyCorrAPI/api/models.py
+++ EasyCorrAPI/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class correlation(models.Model):
+    apiLink1 = models.CharField(max_length=100)
+    name1 = models.CharField(max_length=50)
+    apiLink2 = models.CharField(max_length=100)
+    name2 = models.CharField(max_length=50)
+    correlationVal = models.DecimalField(max_digits=3, decimal_places=2)
+    lastUpdated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name1} and {self.name2}"
