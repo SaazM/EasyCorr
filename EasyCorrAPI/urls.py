8a0b4b8473bbd92c28972f904ec5950a7091ba18
Saaz Mahadkar <saaz.m@icloud.com>
2021-07-08 16:31:15 -0400

    this actually might work

--- EasyCorrAPI/urls.py
+++ EasyCorrAPI/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from social_login import views as v
 from dj_rest_auth.views import LogoutView
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
@@ -25,5 +27,5 @@ urlpatterns = [
     path('dj-rest-auth/facebook/', v.FacebookLogin.as_view(), name='fb_login'),
     path('dj-rest-auth/google/', v.GoogleLogin.as_view(), name='google_login'),
     path('dj-rest-auth/logout/', LogoutView.as_view(), name='logout'),
-
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
