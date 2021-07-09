2b3ad67b99a7840ffce11257c753783ede357b82
Saaz Mahadkar <saaz.m@icloud.com>
2021-07-09 16:15:51 -0400

    nvm keep logout

--- src/components/navbar-out.js
+++ src/components/navbar-out.js
@@ -2,6 +2,17 @@ import React from "react";
 import "../static/css/index.css";
 import "../static/css/bootstrap.css";
 const  NavbarOut = ({loggedin}) => {
+    const logout = () =>{
+        const requestOptions = {
+            method: "POST",
+        }
+        fetch("/dj-rest-auth/logout/", requestOptions).then((response) => 
+            console.log("logged out")
+        )
+        window.location.href = "/";
+
+
+    }
     if(loggedin){
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -17,6 +28,9 @@ const  NavbarOut = ({loggedin}) => {
                         </li>
                         <li className="nav-item">
                         <a className="nav-link" href="/about">About</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/"onClick={logout}>Sign Out</a>
                         </li>
                         <li className="nav-item">
                         </li>
